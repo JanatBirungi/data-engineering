08577d74c785095a1774cca9f2f866dcf797ca55
mono <mkobrocka@gmail.com>
2023-01-18 16:02:16 +0200

    merged older PRs and added virtual env

--- postgres-normalisation-analysis-of-titanic-data/embarked.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE embarked(
-    id  SERIAL NOT NULL PRIMARY KEY,
-    port_of_embarkation TEXT,
-    port_initial TEXT
-);
-INSERT INTO embarked(port_of_embarkation) VALUES ('Cherbourg','C');
-INSERT INTO embarked(port_of_embarkation) VALUES ('Queenstown','Q');
-INSERT INTO embarked(port_of_embarkation) VALUES ('Southampton','S');
-
--- postgres-normalisation-analysis-of-titanic-data/survival.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE survival(
-    id  INT NOT NULL PRIMARY KEY,
-    survived TEXT
-);
-INSERT INTO survival(id,survived) VALUES (0,'No');
-INSERT INTO survival(id,survived) VALUES (1,'Yes');
